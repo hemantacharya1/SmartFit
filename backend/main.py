116dffcacd6ea7c937902cfe222d797e2d70bd74
hemantacharya1 <hemantacharya882@gmail.com>
2025-08-19 18:28:10 +0530

    final

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,24 @@
-from typing import Union
 from fastapi import FastAPI
-from .api import users,progress,nutrition,workout
-app=FastAPI()
+from .api import users, progress, nutrition, workout, exercises, chat 
+from .database import create_db_and_tables
+from . import models 
+
+app = FastAPI(
+    title="SmartFit API",
+    description="Smartfit backend"
+)
+
 app.include_router(users.router)
 app.include_router(progress.router)
 app.include_router(nutrition.router)
 app.include_router(workout.router)
-
+app.include_router(exercises.router) 
+app.include_router(chat.router) 
 
 @app.on_event("startup")
 def on_startup():
-    users.create_db_and_tables()
+    create_db_and_tables()
 
 @app.get('/')
 def hello():
-    return {'Response':"Hello Smartfit"}
+    return {'Response': "Hello Smartfit"}
